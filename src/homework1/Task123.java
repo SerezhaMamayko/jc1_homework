709b24dc696a2051fc760abf83b1b4185c73169c
SerezhaMamayko <sciepanenka@gmail.com>
2023-03-26 22:14:14 +0300

    Task 1,2,3 are done

---
+++ src/homework1/Task123.java
@@ -0,0 +1,8 @@
+package homework1;
+
+public class Task123 {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println("Hello Belarus! " + "I like Java!");
+    }
+}
