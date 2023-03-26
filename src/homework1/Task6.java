d7bf965b2a6c1ec6f8ff9260661ff2805a22ee47
SerezhaMamayko <sciepanenka@gmail.com>
2023-03-26 22:31:57 +0300

    Task 6 is done

---
+++ src/homework1/Task6.java
@@ -0,0 +1,22 @@
+package homework1;
+
+public class Task6 {
+    public static void main(String[] args) {
+        int s = 1000008;
+        int sec;
+        int m;
+        int min, h;
+        sec = s % 60;
+        m = (s-sec) / 60;
+        min = m % 60;
+        h = (m-min) / 60;
+        // System.out.print(h + " " + min + " " + sec + " ");
+        int hour;
+        int d, days, w;
+        hour = h % 24;
+        d = (h-hour) / 24;
+        days = d % 7;
+        w = (d-days) / 7;
+        System.out.print(w + " недель " + days + " дней " + hour + " часов " + min + " минут " + sec + " секунд ");
+    }
+}
