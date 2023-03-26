d42e96c4d2f8b24477111919b8f8587ee21229ca
SerezhaMamayko <sciepanenka@gmail.com>
2023-03-26 22:30:16 +0300

    Task 5 is done

---
+++ src/homework1/Task5.java
@@ -0,0 +1,13 @@
+package homework1;
+
+public class Task5 {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 3;
+        System.out.println(sum(x,y));
+    }
+    static int sum (int a,  int b){
+        return (a+b)*a*b;
+    }
+
+}
