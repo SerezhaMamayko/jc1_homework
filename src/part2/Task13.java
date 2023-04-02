f5768f1a2faddb5790edd24b103174efd04de2ea
SerezhaMamayko <sciepanenka@gmail.com>
2023-04-02 21:26:02 +0300

    Task 13 is done

---
+++ src/part2/Task13.java
@@ -0,0 +1,12 @@
+package part2;
+
+public class Task13 {
+    public static void main(String[] args) {
+        int i = 1; int sum = 1;
+        while (i<=10){
+            sum = sum * i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+}
