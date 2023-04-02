9a0db367eb7b6da8227681f60a8324aa0a7d7ef3
SerezhaMamayko <sciepanenka@gmail.com>
2023-04-03 00:24:06 +0300

    Task 21 is done

--- src/part2/Task21.java
+++ src/part2/Task21.java
@@ -2,9 +2,9 @@ package part2;
 
 public class Task21 {
     public static void main(String[] args) {
-        int[] arr = {99, 2, 3, 4, 5, 9, 9, 6};
+        int[] arr = {1, 2, 99, 4, 5, 9, 9, 6};
         int arrIndex = 0;
-        int arrMax = 0;
+        int arrMax = arr[0];
         for (int i = 0; i < arr.length; i++){
             if (arrMax <= arr[i]){
                 arrMax = arr[i];
