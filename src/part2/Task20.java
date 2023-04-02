56ebbdde702986eb2e51f6981cbf821750ac2c32
SerezhaMamayko <sciepanenka@gmail.com>
2023-04-03 00:03:26 +0300

    Task 20 is done

--- src/part2/Task20.java
+++ src/part2/Task20.java
@@ -4,7 +4,7 @@ public class Task20 {
     public static void main(String[] args) {
         int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-        for (int i = 0; i <= arr.length; i+=2){
+        for (int i = 0; i < arr.length; i+=2){
             System.out.println(arr[i]);
         }
     }
