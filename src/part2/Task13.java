59c0811be9ae3d4f1dd5a62f4fa976b47cf46a9e
SerezhaMamayko <sciepanenka@gmail.com>
2023-04-02 21:30:49 +0300

    Task 13 is done

--- src/part2/Task13.java
+++ src/part2/Task13.java
@@ -2,11 +2,13 @@ package part2;
 
 public class Task13 {
     public static void main(String[] args) {
-        int i = 1; int sum = 1;
+        int i = 1;
+        int sum = 1;
+        System.out.println("Факториал 0=0");
         while (i<=10){
             sum = sum * i;
+            System.out.println("Факториал "+i+"="+sum);
             i++;
         }
-        System.out.println(sum);
     }
 }
