3abf818cbb20a0b49d7f4f6184812a11b46116ca
SerezhaMamayko <sciepanenka@gmail.com>
2023-04-02 21:36:53 +0300

    Task 14 is done

---
+++ src/part2/Task14.java
@@ -0,0 +1,13 @@
+package part2;
+
+public class Task14 {
+    public static void main(String[] args) {
+        int i = 1;
+        int res = 1;
+        while (i<=25){
+            res = res*i;
+            i++;
+        }
+        System.out.println(res);
+    }
+}
