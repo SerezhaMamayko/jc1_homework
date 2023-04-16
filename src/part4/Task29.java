d8ba7c36d3580b0938da2c8c7eeb1e64d5c9e405
SerezhaMamayko <sciepanenka@gmail.com>
2023-04-16 22:48:38 +0300

    Task 29 is done

---
+++ src/part4/Task29.java
@@ -0,0 +1,16 @@
+package part4;
+
+import java.util.Scanner;
+
+public class Task29 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str1 = scanner.nextLine();
+        StringBuilder res = new StringBuilder();
+        String [] words = str1.split("\\s+");
+        for ( String word : words )
+            res.append(word.toCharArray()[word.length() - 1]);
+
+        System.out.println("Result: " + res.toString());
+    }
+}
