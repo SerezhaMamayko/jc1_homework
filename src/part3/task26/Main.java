2e6b1ce57a76b7b38aa3ef1d3a2b9e08bb7868de
SerezhaMamayko <sciepanenka@gmail.com>
2023-04-16 21:56:37 +0300

    Task 26 is done

--- src/part3/task26/Main.java
+++ src/part3/task26/Main.java
@@ -2,7 +2,7 @@ package part3.task26;
 
 public class Main {
     public static void main(String[] args) {
-            Automate automate = new Automate(100, 50, 20);
+            Automate automate = new Automate(110, 50, 20);
             automate.printMoneyCount();
             int balance = automate.readBalance();
             automate.moneyOutput(balance);
