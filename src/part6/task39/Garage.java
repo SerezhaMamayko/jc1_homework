294b7d0f2d6cf9ff51478c84282cc6db76726b7a
SerezhaMamayko <sciepanenka@gmail.com>
2023-06-05 19:34:38 +0300

    Task 39 is done

---
+++ src/part6/task39/Garage.java
@@ -0,0 +1,16 @@
+package part6.task39;
+
+import java.util.Scanner;
+
+public class Garage <T extends Vehicle>  {
+    public String name;
+    public String getName() {
+        return name;
+    }
+
+    public void enterVeh (){
+        System.out.println("Enter car or moto: ");
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.nextLine();
+    }
+}
