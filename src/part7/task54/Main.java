39f15f68cb8f8bacba579526f63163708939c207
SerezhaMamayko <sciepanenka@gmail.com>
2023-06-06 16:50:41 +0300

    Task 54 is done

---
+++ src/part7/task54/Main.java
@@ -0,0 +1,26 @@
+package part7.task54;
+
+public class Main {
+    public static void main(String[] args) {
+        NewThread thread1 = new NewThread();
+        thread1.start();
+        NewThread thread2 = new NewThread();
+        thread2.start();
+        NewThread thread3 = new NewThread();
+        thread3.start();
+        NewThread thread4 = new NewThread();
+        thread4.start();
+        NewThread thread5 = new NewThread();
+        thread5.start();
+        NewThread thread6 = new NewThread();
+        thread6.start();
+        NewThread thread7 = new NewThread();
+        thread7.start();
+        NewThread thread8 = new NewThread();
+        thread8.start();
+        NewThread thread9 = new NewThread();
+        thread9.start();
+        NewThread thread0 = new NewThread();
+        thread0.start();
+    }
+}
